d6af732f9c7af0c727f6a6c2439c335a62d2d93a
Joshua Sierles <joshua@diluvia.net>
2015-05-14 20:42:38 +0200

    add docker rebuild/launch script

---
+++ rebuild.sh
@@ -0,0 +1,4 @@
+sudo docker build -t packager .
+sudo docker stop packager
+sudo docker rm packager
+sudo docker run -d -e VIRTUAL_HOST=packager.rnplay.org -v /home/app/rails/rnplay/play_js:/js --name packager packager
